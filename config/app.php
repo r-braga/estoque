b3192d389ae6787aeaa7c1d414507f3587143beb
Rousseau Lobo Braga <rbraga@fatec-ro.br>
2015-08-15 14:44:38 -0400

    Alterado o namespace do projeto para estoque

--- config/app.php
+++ config/app.php
@@ -140,9 +140,9 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        estoque\Providers\AppServiceProvider::class,
+        estoque\Providers\EventServiceProvider::class,
+        estoque\Providers\RouteServiceProvider::class,
 
     ],
 
