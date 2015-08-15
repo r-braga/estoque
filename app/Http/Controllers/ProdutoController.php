3ad7d8cbc52539ae40a91972faff0f9d23b06f12
Rousseau Lobo Braga <rbraga@fatec-ro.br>
2015-08-15 15:13:58 -0400

    Adicionado o acesso a view pelo metodo with para enviar os produtos para a view

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -5,15 +5,8 @@ use Illuminate\Support\Facades\DB;
 class ProdutoController extends Controller {
     public function lista(){
         // código aqui 
-        $html = '<h1>Listagem de produtos Laravel</h1>';
-        $html .= '<ul>';
         $produtos = DB::select('select * from produtos');
-        foreach ($produtos as $p) {
-            $html .= '<li> Nome: '. $p->nome .',
-                    Descrição: '. $p->descricao . '</li>';
-        }
-        $html .= '</ul>';
         
-        return $html;
+        return view('listagem')->with('produtos',$produtos);
     }
 }
